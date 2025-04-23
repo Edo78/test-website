65f06ae9cec8cf2e74f975dc11b7e664ac121c2f
Federico "Edo" Granata <edo78@noreply.gitea.federico.granata.dev>
2025-04-23 09:33:28 +0200

    Comment away tinacloud values

--- tina/config.ts
+++ tina/config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "tinacms";
 
 export default defineConfig({
-  clientId: process.env.TINA_PUBLIC_CLIENT_ID || "",
-  token: process.env.TINA_TOKEN || "",
-  branch: "master",
+  // clientId: process.env.TINA_PUBLIC_CLIENT_ID || "",
+  // token: process.env.TINA_TOKEN || "",
+  // branch: "master",
   build: {
     outputFolder: "admin",   // cartella generata per l’UI di Tina
     publicFolder: "public",  // cartella degli asset pubblici
